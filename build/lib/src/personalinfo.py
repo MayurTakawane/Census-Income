7adaaa099d6efb7f880690bff3d4765067a6ec10
Mayur <mayurtakawane195@gmail.com>
2023-08-10 16:27:00 +0530

    modified

---
+++ build/lib/src/personalinfo.py
@@ -0,0 +1,7 @@
+def get_user(self):
+    user = 'mayurtakawane'
+    return user
+
+def get_password(self):
+    password = 'dogs1818'
+    return password
---
+++ src/personalinfo.py
@@ -0,0 +1,8 @@
+def get_user():
+    user = 'mayurtakawane'
+    return user
+
+def get_password():
+    password = 'dogs1818'
+    return password
+
